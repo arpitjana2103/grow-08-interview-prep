45a7eb277d1262fa1c2c41eb4e2b464832ee8b1a
Arpit Jana <arpitjana2103@gmail.com>
2026-03-13 19:12:07 +0530

    learning linked list

--- fe-interview/dsa/math/fibonacci.js
+++
@@ -1,9 +0,0 @@
-// Q2 : Fibonacci Number
-// Fibonacci Series => 0, 1, 1, 2, 3, 5, 8, 13
-
-function fibonacci(n) {
-	if (n <= 1) return n;
-	return fibonacci(n - 1) + fibonacci(n - 2);
-}
-
-console.log(fibonacci(10));
